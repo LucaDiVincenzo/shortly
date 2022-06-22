31ea56ca798440a472b6c0bc141f002a4e076c68
Emanuele Russo <emanuele.panico.russo@gmail.com>
2022-06-22 17:45:22 +0200

    Finish all js logics

--- scripts.js
+++ scripts.js
@@ -13,10 +13,6 @@ const access = {
     submit: document.querySelector('.page-middle button'),
 }
 
-const variables = {
-    lastLink: 'Property-value to be overwritten',
-}
-
 function addAlert() {
     access.shorten.classList.add('invalid-link');
     access.invText.style.display = 'block';
@@ -27,35 +23,55 @@ function remAlert() {
     access.invText.style.display = 'none';
 }
 
+function addCopy() {
+    const copyBtn = document.querySelector('.copy:first-of-type');
+    copyBtn.addEventListener('click', (e) => {
+        const copyUrl = e.target.previousElementSibling;
+        navigator.clipboard.writeText(copyUrl.innerText)
+            .then(function () {
+                e.target.classList.add('copied');
+                e.target.innerText = 'Copied!';
+                setTimeout(() => {
+                        e.target.classList.remove('copied');
+                        e.target.innerText = 'Copy'
+                    }, 750);
+            });
+    });
+}
+
 function isValidUrl(string) {
     if (!/^https?:\/\//i.test(string)) {
-        string = 'https://' + string;
-    }
+        string = 'https://' + string; }
     const matchPattern = /^(?:\w+:)?\/\/([^\s.]+\.\S{2}|localhost[:?\d]*)\S*$/;
     return matchPattern.test(string);
 }
 
-function getLink(object) {
-    variables.lastLink = object["result"]["short_link"];
-}
-
-function shortUrl() {
-    const apiBase = 'https://api.shrtco.de/v2/shorten?url=';
-    const apiUrl = apiBase + access.input.value;
-    fetch(apiUrl)
-        .then(rsp => rsp.json())
-        .then(data => getLink(data))
-}
-
-function addLink(text) {
+function addRow(url) {
     const newElement = document.createElement('div');
     newElement.className = `link`;
     document.getElementById('shorten').after(newElement);
     newElement.innerHTML = `
     <div class="new_container">
-        <p>${text}</p>
+        <p>${url}</p>
+        <p class="short"></p>
         <button class="copy">Copy</button>
     </div>`;
+    addCopy();
+}
+
+function addShort(data) {
+    const shortLink = document.querySelector('.short');
+    console.log(data);
+    shortLink.innerText = data['result']['short_link'];
+}
+
+function shortUrl() {
+    const apiBase = 'https://api.shrtco.de/v2/shorten?url=';
+    const apiUrl = apiBase + access.input.value;
+    addRow(access.input.value);
+    fetch(apiUrl)
+        .then(rsp => rsp.json())
+        .then(data => addShort(data));
 }
 
 function linkSubmit() {
@@ -66,8 +82,6 @@ function linkSubmit() {
             remAlert();
         }
         shortUrl();
-        addLink(access.input.value);
-        // please, after submitting check 'variables.lastLink' in web console
     } else {
         if (alertChecker !== undefined) {
             access.input.value = null;
